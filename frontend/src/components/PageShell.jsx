8e95f78162a2d03a34070af1d241cba035fa82df
Sujal Mahapatra <159683157+SujalMahapatra@users.noreply.github.com>
2026-09-11 19:18:41 +0530

    Fix Tailwind Typo for deployment

--- frontend/src/components/PageShell.jsx
+++ frontend/src/components/PageShell.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import BackgroundFX from "../components/BackgroundFX";
+import BackgroundFX from "../components/BackgroundFx";
 import CopilotWidget from "./CopilotWidget";
 
 export const PageShell = ({ children, variant = "app", testId }) => {
